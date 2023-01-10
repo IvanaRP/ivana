94515d3f7d1a61386445ec10575db6f1e0220f69
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2023-01-10 16:44:43 +0100

    create React
    pages
    links

---
+++ ivanaapp/src/Pages/Home/index.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import React from "react";
+
+// import "../../styles/home.css";
+// import EmployeesForm from "../../Components/Form";
+
+function Home() {
+  return (
+    <div className="homeWrapper">
+      <div className="homeContainer">
+        <h1>HRnet</h1>
+        <div className="home-formContainer">
+          <Link to="/employee-list" className="homeWrapper-link">
+            View Current Employees dasdkasd
+          </Link>
+          <h2>Create Employee</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
