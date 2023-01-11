c3cb6b3bc5fc0af4f14c5a85e863508bbd839858
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2023-01-11 17:17:44 +0100

    css
    css home page

--- ivanaapp/src/Pages/Home/index.jsx
+++ ivanaapp/src/Pages/Home/index.jsx
@@ -3,22 +3,28 @@ import React from "react";
 
 import Navigation from "../../Components/Navigation";
 import "../../styles/home.css";
+import logoIV from "../../assets/logoIV.png";
 
 function Home() {
   return (
     <>
-      <Navigation></Navigation>
+      <div className="globalWrapper">
+        <Navigation></Navigation>
 
-      <div className="homeWrapper">
-        <div className="homeContainer">
-          <h1>Hej I’m Ivana !</h1>
-          <h2>
-            Application developer, focused on beautiful and USEFUL APP.
-            Currently based in Copenhagen, Denmark.
-            <Link to="/employee-list" className="homeWrapper-link">
-              This is my potfolio.
-            </Link>
-          </h2>
+        <div className="homeWrapper">
+          <div className="homeContainer">
+            <img src={logoIV} className="logo" alt="logo" />
+            <h1>Hej I’m Ivana !</h1>
+            <h2>
+              Application developer, focused on beautiful and USEFUL APP.
+              Currently based in Copenhagen, Denmark.
+            </h2>
+            <h2>
+              <Link to="/employee-list" className="homeWrapper-link">
+                This is my potfolio.
+              </Link>
+            </h2>
+          </div>
         </div>
       </div>
     </>
